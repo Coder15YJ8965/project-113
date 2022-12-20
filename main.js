4d732a43d22f1981a42ab841eb8f99080af89be4
Coder15YJ8965 <86280451+Coder15YJ8965@users.noreply.github.com>
2022-12-20 04:22:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110,250);
+    video= createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    createCanvas(150, 150)
+    circle(30, 30, 20)
+}
